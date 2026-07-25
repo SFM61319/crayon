2e63fed580bf141c0c3f275a276314d9b6d0e06c
Avinash Maddikonda <45308169+SFM61319@users.noreply.github.com>
2026-07-25 19:48:49 +0530

    chore(lib): Setup and configure CMake

---
+++ include/crayon/crayon.hpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2026 Avinash Maddikonda
+// SPDX-License-Identifier: Apache-2.0
+
+#pragma once
+
+#include <string>
+
+/// @brief Hello, Crayon!
+/// @return "Hello, Crayon!"
+std::string hello_crayon();
---
+++ src/crayon.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2026 Avinash Maddikonda
+// SPDX-License-Identifier: Apache-2.0
+
+#include <string>
+
+#include <crayon/crayon.hpp>
+
+std::string hello_crayon() { return "Hello, Crayon!"; }
